462e942c3e3f7fdb1887e70df1c9e13c8d78eabc
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-03 13:43:19 +0900

    Add: 「きゅうり」ボタンクリックでフィルタリング

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const elem = document.querySelector('.grid');
   const fragment = document.createDocumentFragment();
 
+  const btnAll = document.querySelector('.all');
+  const btnCucumber = document.querySelector('.cucumber');
+  const btnCorn = document.querySelector('.corn');
+  const btnTomato = document.querySelector('.tomato');
+  const btnEggplant = document.querySelector('.eggplant');
+  const btnGreenpepper = document.querySelector('.greenpepper');
+
   const JSONPath = '/JSON/image.json';
   const jsonData = fetch(JSONPath).then((response) => {
     return response.json();
@@ -23,6 +30,35 @@ document.addEventListener('DOMContentLoaded', function () {
     const grid = new Muuri(elem, {
       layoutOnResize: 10,
       layoutDuration: 600,
+    });
+  });
+
+  btnCucumber.addEventListener('click', function () {
+    const items = document.querySelectorAll('.item');
+    items.forEach((item) => {
+      item.remove();
+    });
+    jsonData.then((jsonData) => {
+      console.log(jsonData);
+      const cucumberArray = jsonData.filter((item, i, array) => {
+        return item['type'] === 'cucumber';
+      });
+      cucumberArray.forEach((data) => {
+        const item = document.createElement('div');
+        const img = document.createElement('img');
+        item.className = 'item';
+        img.className = 'item-content';
+        img.src = data['path'];
+        img.width = data['width'];
+        img.height = data['height'];
+        item.appendChild(img);
+        fragment.appendChild(item);
+      });
+      elem.appendChild(fragment);
+      const grid = new Muuri(elem, {
+        layoutOnResize: 10,
+        layoutDuration: 600,
+      });
     });
   });
 });
